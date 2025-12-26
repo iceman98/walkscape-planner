1f0b6932142cc073d20ab1029c70c85714b9d777
David <dvillegas@hotelbeds.com>
2025-12-26 09:57:10 +0100

    add example recipes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-    import { browser } from "$app/environment";
-    import Stage from "svelte-konva/Stage.svelte";
-    import Layer from "svelte-konva/Layer.svelte";
-    import Rect from "svelte-konva/Rect.svelte";
-    import Text from "svelte-konva/Text.svelte";
-	import data from '../data/recipes.json';
-  import character from '../data/character.json';
+  import { browser } from "$app/environment";
+  import { Stage, Layer, Group, Text, Rect } from "svelte-konva";
 
+  import character from "../data/character.json";
+  import recipes from "../data/recipes.json";
 </script>
 
 {#if browser}
-<Stage width={window.innerWidth} height={window.innerHeight}>
-  <Layer>
-    {#each data.recipes as recipe}
-        <Text text={recipe.name} x={100} y={100} />
-    {/each}
-  </Layer>
-</Stage>
+  <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Layer>
+      {#each recipes as recipe, index}
+        <Group x={10} y={20 * index}>
+          <Rect x={0} y={0} width={100} height={20} fill="grey" />
+          <Text text={recipe.name} x={0} y={0} color="white" />
+        </Group>
+      {/each}
+    </Layer>
+  </Stage>
 {/if}
